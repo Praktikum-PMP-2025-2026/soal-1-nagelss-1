dae707f1389ba0220c5ab63e383a7ff5000c832c
nagelss <hufnagelrls@gmail.com>
2026-04-21 09:20:50 +0700

    commit1.c

--- soal1.c
+++ soal1.c
@@ -1,5 +1,75 @@
-#include <stdio.h>
+/** EL2008 Praktikum Pemecahan Masalah dengan Pemrograman 2024/2025
+ *   Modul               : 2
+ *   Hari dan Tanggal    : Selasa, 21 April 2026
+ *   Nama (NIM)          : Hufnagel Ruben Lenard Situmorang (13224015)
+ *   Nama File           : soal1.c
+ *   Deskripsi           : Menghapus seminimalpun karakter kurung dari string
+ * 
+ */
 
-int main(void) {
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(){
+    char script[100];
+    char new[100];
+
+    scanf("%s",&script);
+
+    int delete;
+    int x = 0;
+    int j;
+
+    for (int i=0;i<strlen(script);i++){
+        delete = 0;
+        if (script[i]=='('){
+            j = i+1;
+            if (j==strlen(script)){
+                delete = 1;
+            }
+            else{
+                for (int j=i+1;j<strlen(script);j++){
+                    if (script[j]=='('){
+                        delete = 1;
+                        break;
+                    }
+                    else if (script[j]==')'){
+                        delete = 0;
+                        break;
+                    }
+                    else{
+                        delete = 1;
+                    }
+                }
+            }
+        }
+        else if (script[i]==')'){
+            j = i-1;
+            if (j==-1){
+                delete = 1;
+            }
+            else{
+                for (int j;j>-1;j--){
+                    if (script[j]=='('){
+                        delete = 0;
+                        break;
+                    }
+                    else if (script[j]==')'){
+                        delete = 1;
+                        break;
+                    }
+                    else{
+                        delete = 1;
+                    }
+                }
+            }
+        }
+        if (delete==0){
+            new[x] = script[i];
+            x += 1;
+        }
+    }
+    printf("%s",new);
     return 0;
 }
